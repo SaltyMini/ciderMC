df059edc8550ecebea40933d70b25d77172bee65
SaltyMini <samclaymichael@gmail.com>
2025-03-13 03:09:32 +0000

    Setup for rank command

---
+++ src/main/java/com/cidermc/untitled/commands/subCommands/ranks.java
@@ -0,0 +1,37 @@
+package com.cidermc.untitled.commands.subCommands;
+
+import com.cidermc.untitled.commands.commandStruct;
+import org.bukkit.command.CommandSender;
+
+public class ranks extends commandStruct {
+    @Override
+    public String getName() {
+        return "ranks";
+    }
+
+    @Override
+    public String getDescription() {
+        return "ranks manager";
+    }
+
+    @Override
+    public String commandType() {
+        return "rank";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "";
+    }
+
+    @Override
+    public void commandRun(CommandSender commandSender, String[] args) {
+
+        //open gui
+
+
+        //gui made of 15 elements
+
+
+    }
+}
