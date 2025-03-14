971c61515b1bd11e7af0a8cdce3b3c6b6dc3bfdf
SaltyMini <samclaymichael@gmail.com>
2025-03-14 19:57:12 +0000

    Added hasRequiredMoney and hasRequiredMCMMO method

--- src/main/java/com/cidermc/untitled/gui/rankPlayerHandle.java
+++ src/main/java/com/cidermc/untitled/gui/rankPlayerHandle.java
@@ -7,7 +7,26 @@ import java.net.http.WebSocket;
 
 public class rankPlayerHandle implements Listener {
 
+    public static boolean hadRequirementMoney(Player player, String[] requirements) {
+
+        String moneyRequirment = requirements[0];
+        moneyRequirment = moneyRequirment.substring(1); //Remove $ sign
+
+        
+        return true;
+    }
+
+    public static boolean hadRequirementMCMMO(Player player, String[] requirements) {
+
+        String moneyRequirment = requirements[0];
+        moneyRequirment = moneyRequirment.substring(1); //Remove $ sign
+
+
+        return true;
+    }
+
     public static void rankPlayerHandle(Player player, String rank, String[] requirments, String... lore) {}
+
 
 
 
