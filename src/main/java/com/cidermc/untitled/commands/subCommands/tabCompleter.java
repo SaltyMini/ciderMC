5b1591a6c01924dcbc7127e18246ac881f999bd7
SaltyMini <samclaymichael@gmail.com>
2025-03-20 01:14:00 +0000

    Added tab completer for inventory count command

---
+++ src/main/java/com/cidermc/untitled/commands/subCommands/tabCompleter.java
@@ -0,0 +1,32 @@
+package com.cidermc.untitled.commands.subCommands;
+
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class tabCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, Command command, String alias, String[] args) {
+        if(!command.getLabel().equalsIgnoreCase("inventorycount")
+                || !alias.equalsIgnoreCase("inventorycount")
+                || !(commandSender instanceof Player player)) {
+            return null;
+        }
+        if(args.length >= 1) {
+            return null;
+        }
+
+        if(!player.hasPermission("cider.inventorycount.other")) {
+            return null;
+        }
+
+        return List.of(Arrays.toString(Material.values()));
+    }
+}
