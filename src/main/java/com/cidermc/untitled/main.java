b17cab88345d909c2fef1da948197cbe2d88856f
SaltyMini <64852397+SaltyMini@users.noreply.github.com>
2025-03-23 01:05:03 +0000

    Added config generation

--- src/main/java/com/cidermc/untitled/main.java
+++ src/main/java/com/cidermc/untitled/main.java
@@ -8,12 +8,15 @@ import com.cidermc.untitled.gui.RanksGUI;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.xenondevs.invui.InvUI;
 
+import java.io.File;
 import java.util.Objects;
 
 public final class main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        generateConfigs();
+
         Objects.requireNonNull(getCommand("cider")).setExecutor(new CommandManager());
         getServer().getPluginManager().registerEvents(new RanksGUI(), this);
     // runs on start
@@ -32,5 +35,25 @@ public final class main extends JavaPlugin {
         currentEvent eventInstance = currentEvent.getInstance(this);
         eventInstance.saveScore();
 
+    }
+
+    private void generateConfigs() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+
+        saveDefaultConfig();
+
+        String[] configFiles = {
+                "events.yml",
+                "playerScores.yml"
+        };
+
+        for (String fileName : configFiles) {
+            File configFile = new File(getDataFolder(), fileName);
+            if (!configFile.exists()) {
+                saveResource(fileName, false);
+            }
+        }
     }
 }
